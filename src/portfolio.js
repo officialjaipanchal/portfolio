bf6eb11b53debbed04e681e0c8a6777bdbc491bf
Jerry <jerry@Jerrys-MacBook-Air.local>
2023-11-30 11:36:34 -0700

    V.1.1.2

--- src/portfolio.js
+++ src/portfolio.js
@@ -129,7 +129,7 @@ const educationInfo = {
       subHeader: "Master of Technology in Computer Engineering",
       duration: "September 2020 - June 2022",
       desc: "I have wrriten a research paper on Machine Learning Based Malware Evaluation for Android, Got Second Ranked in the University",
-      descBullets: ["CGPA : 8.36/10"]
+      // descBullets: ["CGPA : 8.36/10"]
     },
     {
       schoolName: "Ganpat University",
@@ -137,7 +137,7 @@ const educationInfo = {
       subHeader: "Bachelor of Technology in Computer Engineering",
       duration: "September 2017 - June 2020",
       desc: "I have wrriten a research paper on Machine Learning Based Malware Evaluation for Android, Got Second Ranked in the University",
-      descBullets: ["CGPA : 7.52/10"]
+      // descBullets: ["CGPA : 7.52/10"]
     }
   ]
 };
@@ -152,6 +152,10 @@ const techStack = {
       progressPercentage: "80%" //Insert relative proficiency in percentage
     },
     {
+      Stack: "Data Analytics", //Insert stack or technology you have experience in
+      progressPercentage: "78%" //Insert relative proficiency in percentage
+    },
+    {
       Stack: "Web Development",
       progressPercentage: "90%"
     },
@@ -162,7 +166,16 @@ const techStack = {
     {
       Stack: "Machine Learning / Artificial Intelligence",
       progressPercentage: "66%"
+    },
+    {
+      Stack: "Database",
+      progressPercentage: "86%"
+    },
+    {
+      Stack: "Operating Systems: MacOS, Windows, Linux",
+      progressPercentage: "95%"
     }
+
   ],
   displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
 };
@@ -172,6 +185,17 @@ const techStack = {
 const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
+    {
+      role: "Teaching Assistant",
+      company: "Arizona State University",
+      companylogo: require("./assets/images/ASU LOGO.png"),
+      date: "Dec 2023 - Working",
+      // desc: " ",
+      descBullets: [
+        "Supporting the professor in assignment creation, practical documentation.",
+        "Grading Student Submission",
+      ]
+    },
     {
       role: "Software Developer",
       company: "Srashtasoft",
@@ -221,10 +245,58 @@ const openSource = {
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Game Projects",
-  subtitle: "Here are some of the games I've developed during my tenure at Srashtasoft Company.",
+  title: "Projects",
+  subtitle: "Here are some of the Project I've developed.",
   projects: [
     {
+      image: require("./assets/images/aws.png"),
+      projectName: "Secure Static Web Application",
+      projectDesc: "Secured website using AWS suite: EC2, Load Balancer, CloudWatch, KMS, IAM, and VPN for robust protection",
+      footerLink: [
+        {
+          name: "Website",
+          url: "http://ift520project.edu.s3-website-us-east-1.amazonaws.com/"
+        }, 
+      ]
+    },
+    {
+      image: require("./assets/images/android_malware.png"),
+      projectName: "MACHINE LEARNING BASED MALWARE EVALUATION FOR ANDROID",
+      projectDesc: "This project evaluates diverse machine learning models like SVM, KNN, and Decision Trees, utilizing static APK features. Its goal is to distinguish malware from benign Android apps, offering crucial insights to combat security threats in the Android ecosystem.",
+      footerLink: [
+        {
+          name: "Research Paper",
+          url: "https://drive.google.com/file/d/11qfqP6qBCc8urk1pP26YhTkL67ePnTgi/view?usp=sharing"
+        }, 
+      ]
+    },
+    {
+      image: require("./assets/images/shopping.png"),
+      projectName: "Ecommerce Shopping ",
+      projectDesc: "Amid the COVID-19 lockdown, my friend and I developed a website to help people order essential groceries conveniently. The platform consisted of two modules: Admin and Users. Users could place orders for daily products, and our system ensured doorstep delivery for their convenience",
+    },
+    {
+      image: require("./assets/images/cleaning.png"),
+      projectName: "Ecommerce Cleaning",
+      projectDesc: "During my internship, I developed NE Cleaning, a website with three modules: Admin, Client, and Worker. Clients could easily visit the site and book cleaning services for their homes. The system operated on a first-come, first-served basis, where nearby workers could accept service requests and complete the cleaning. The admin module handled order management, payments, and user administration.",
+    },
+    {
+      image: require("./assets/images/mindshift.jpeg"),
+      projectName: "Mindshift Motors",
+      projectDesc: "Crafted an engaging brain puzzle game that puts players in control of two cars simultaneously, challenging their multitasking abilities as they skillfully maneuver the vehicles to avoid obstacles and prevent any collisions.",
+      footerLink: [
+        {
+          name: "Play Store",
+          url: "https://play.google.com/store/apps/details?id=com.apps.srashtasoft.mindshiftmotors"
+        },
+        {
+          name: "App Store",
+          url: "https://apps.apple.com/us/app/mindshift-motors/id6449936547"
+        }
+        // You can add extra buttons here.
+      ]
+      
+    },{
       image: require("./assets/images/2048.webp"),
       
       projectName: "2048 Infinite",
@@ -237,9 +309,12 @@ const bigProjects = {
         {
           name: "App Store",
           url: "https://apps.apple.com/us/app/2048-infinite/id6448166018"
-        }
+        },
         // You can add extra buttons here.
-        
+        {
+          name: "#Unity",
+          fontAwesomeClassname: "fab fa-unity"
+        }
       ]
       
     },
@@ -342,59 +417,62 @@ const bigProjects = {
 const achievementSection = {
   title: emoji("Achievements And Certifications 🏆 "),
   subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+    "Achievements, Certifications Some Cool Stuff that I have done !",
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "AZNext Introduction to Robotics in Microelectronics Manufacturing Training Program",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "Earned Competencies: Cleanroom Robotics, Design Attributes of Atmospheric Robots, Design of Vacuum Robots, Kinematics of Industrial Robots, Dynamics and Control of Industrial Robots, Test and Characterization,        ",
+      image: require("./assets/images/Azrobot.png"),
+      imageAlt: "Certification Robotics & ML",
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+          url: "https://badgr.com/public/assertions/piDsMdl2Td2Mpk_qjfy_Zw"
         },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-        }
+       
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "AWS Certified Security-Specialty (2022)",
       subtitle:
         "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
+      image: require("./assets/images/cybrary_logo.jpeg"),
+      imageAlt: "AWS Certification",
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Cybrary",
+          url: "https://app.cybrary.it/courses/api/certificate/CC-748a7bb7-929a-423a-b8e3-8b94a3b20e3a/view"
         }
       ]
     },
-
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
+      title: "Fundamentals of Digital Marketing",
+      subtitle: "Completed Certifcation from Google for Digital Marking, Credential Id: 3ME 7S7 CXJ",
+      image: require("./assets/images/google.jpeg"),
+      imageAlt: "Google Certification",
       footerLink: [
-        {name: "Certification", url: ""},
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Fundamentals of Digital Marketing",
+          url: "https://learndigital.withgoogle.com/digitalunlocked/validate-certificate-code"
+        }
+      ]
+    },
+    {
+      title: "LinkedIn Learning Certificate of Completion",
+      subtitle: "Completed Certifcation from Linkedin for Introduction to Career Skills in Data Analytics",
+      image: require("./assets/images/linkedin.png"),
+      imageAlt: "Linkedin Certificate",
+      footerLink: [
+        {
+          name: "Data Analytics",
+          url: "https://www.linkedin.com/learning/certificates/1112518e484f6a640fb06693ea94e538aaa41a7cfc55100ec464c53998c11727"
         }
       ]
     }
   ],
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Blogs Section
@@ -406,19 +484,19 @@ const blogSection = {
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "#",
-      title: "Title",
+      url: "https://dotbatcode.blogspot.com/",
+      title: "Bat Files",
       description:
-        "Description"
+        "In this blog, I discuss BAT code—an executable script in Windows that stores commands to be executed sequentially"
     },
     {
-      url: "#",
-      title: "title",
+      url: "https://alltoolhere.blogspot.com/",
+      title: "All Tool Here",
       description:
-        "Description"
+        "In 2018, I created this blog out of necessity as I encountered difficulties finding software easily on the internet. To address this, I uploaded software to links and shared them through this blog."
     }
   ],
-  display: false // Set false to hide this section, defaults to true
+  display: true // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -466,7 +544,7 @@ const twitterDetails = {
   display: false // Set true to display this section, defaults to false
 };
 
-const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
